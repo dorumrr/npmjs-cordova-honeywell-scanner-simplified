06075e8632324932e08c410726224fb8cc056e5f
Diego Dias <diego.dfsd@gmail.com>
2019-06-30 19:47:35 -0300

    Add imports

--- src/android/HoneywellScannerPlugin.java
+++ src/android/HoneywellScannerPlugin.java
@@ -1,5 +1,10 @@
 package com.icsfl.rfsmart.honeywell;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import android.content.Context;
 import android.util.Log;
 
